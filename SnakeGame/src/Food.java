14f337ddcb9d29998c895547bcad5ae8a360bde1
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-03-27 16:27:55 +0700

    quý

--- SnakeGame/src/Food.java
+++ SnakeGame/src/Food.java
@@ -5,13 +5,24 @@ public class Food {
     private int XFood;
     private int YFood;
     public Food(){
-        for (int i=0;i<Game.snakes.size();i++){
-            do{
-                Random r = new Random();
-                XFood = r.nextInt(13)+1;
-                YFood = r.nextInt(13)+1;
-            }while (Game.food.getFood()==Game.snakes.get(i).getDot());
-        }
+
+        do {
+            Random r = new Random();
+            XFood = r.nextInt(13) + 1;
+            YFood = r.nextInt(13) + 1;
+//           Check moi khong == than
+            int q=0;
+            for(int i=0;i<Game.snakes.size();i++){
+                if(!(XFood==Game.snakes.get(i).getDot_x() && YFood==Game.snakes.get(i).getDot_y() )){
+                    q++;
+                }
+            }
+            if (q==Game.snakes.size()){
+                break;
+            }
+
+        }while (true);
+
     }
 
     public int[] getFood() {
