e69e2402aa851874b1f46978ed0fcdbcdd507915
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-04-19 09:14:08 +0700

    bài thi

--- BTK_Java_I/src/EX2/Flight.java
+++ BTK_Java_I/src/EX2/Flight.java
@@ -5,12 +5,18 @@ public class Flight {
     private String destination;
 
     public Flight() {
+        destination = "";
         number =0;
     }
-
     public Flight(int number, String destination) {
-        this.number = number;
-        this.destination = destination;
+        if(number>0 && !destination.equals(null)){
+            this.number = number;
+            this.destination = destination;
+        }else {
+            this.number = 0;
+            this.destination = "";
+        }
+
     }
 
     public void display(){
--- BTK_Java_I/src/EX2/TestFight.java
+++ BTK_Java_I/src/EX2/TestFight.java
@@ -4,5 +4,7 @@ public class TestFight {
     public static void main (String args []) {
         Flight gl = new Flight (857, "Toronto");
         gl.display ();
+        System.out.println("Flight number is: " + gl.getNumber());
+        System.out.println("Destination is: "+gl.getDestination());
     }
 }
